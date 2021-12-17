3d7bcee57dad194c78bd5a5ae79334ea177f07ef
= <=>
2021-12-17 16:06:57 +0000

    updates

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_netflix_responsive_ui/widgets/widgets.dart';
+import '../data/data.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  ScrollController _scrollController;
+  double _scrollOffset = 0.0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _scrollController = ScrollController()
+      ..addListener(() {
+        setState(() {
+          _scrollOffset = _scrollController.offset;
+        });
+      });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey[850],
+        child: const Icon(Icons.cast),
+        onPressed: () => print('cast'),
+      ),
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 50.0),
+        child: CustomAppBar(
+          scrollOffset: _scrollOffset,
+        ),
+      ),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          SliverToBoxAdapter(
+            child: ContentHeader(featuredContent: sintelContent),
+          )
+        ],
+      ),
+    );
+  }
+}
